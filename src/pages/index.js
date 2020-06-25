35ef115502344dd0dd8f6139d257f2152f2173dd
George Stark <georgestark@georges-mbp.home>
2020-06-25 17:18:13 +1000

    Changes to fix author details

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,7 +65,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
+    allContentfulPerson(filter: { contentful_id: { eq: "2ntJSlMVNqYP4p2XCO6JDH" } }) {
       edges {
         node {
           name
