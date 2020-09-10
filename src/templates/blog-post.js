06f24a0d0e0cd3afaf9524c709ee0cb9bf97e4ba
George Stark <georgestark@georges-mbp.home>
2020-09-10 14:28:09 +1000

    changes for SEO.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 import heroStyles from '../components/hero.module.css'
 
@@ -11,11 +12,22 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const description = get(this.props, 'data.contentfulBlogPost.description')
+
 
     return (
       <Layout location={this.props.location} >
         <div style={{ background: '#fff' }}>
           <Helmet title={`${post.title} | ${siteTitle}`} />
+          <SEO
+            title={`${post.title} | ${siteTitle}`}
+            description={post.description.childMarkdownRemark.html}
+            image={post.heroImage.fluid}
+            pathname={this.props.location.pathname}
+          />
+
+
+
           <div className={heroStyles.hero}>
             <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
           </div>
@@ -55,6 +67,11 @@ export const pageQuery = graphql`
       heroImage {
         fluid(maxWidth: 1180, background: "rgb:000000") {
           ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      description {
+        childMarkdownRemark {
+          html
         }
       }
       body {
