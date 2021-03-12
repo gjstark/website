d27a4acc4248c4bb8ccf48fa839b80e04bc5b9a5
George Stark <georgejstark@gmail.com>
2021-03-12 11:25:25 +1100

    removed the captcha that doesnt work

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,9 +13,9 @@ const ContactPage = () => (
         <h2>Contact Form</h2> 
             Please use the below form to contact us for any enquires you may have. 
 
-        <form method="POST" name="Contact" action="/thankyou/" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
+        <form method="POST" name="Contact" action="/thankyou/" netlify-honeypot="bot-field" data-netlify="true">
           <input type="hidden" name="form-name" value="Contact" />
-          <input type="hidden" name="bot-field"/>
+          <input type="hidden" name="bot-field" />
           <p>
               <label for="name">Name:</label>
               <input type="text" name="name" id="name"/>
@@ -28,7 +28,6 @@ const ContactPage = () => (
               <label for="comments">Comments:</label><br/>
               <textarea name="comments" id="comments"></textarea>
           </p>
-          <div data-netlify-recaptcha="true"></div>
           <p>
               <input type="submit"/>
           </p>
